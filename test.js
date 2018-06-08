3384d38d58fb702ee79f32f55f185636d1ecbe77
Felipe <feliperohdee@gmail.com>
2018-06-08 10:39:41 -0300

    added wrong test

--- test.js
+++ test.js
@@ -5,5 +5,15 @@ const {
     decrypt
 } = crypto('mcpT)(_C-5n=nv"/t~4zBGtnx8/.YTSU');
 
+const {
+    decrypt: wrongDecrypt
+} = crypto('mcpT)(_C-5n=nv"/t~4zBGtnx8/.YTSV');
+
 assert.strictEqual(decrypt(encrypt('encrypt')), 'encrypt');
 assert.strictEqual(decrypt(encrypt('mcpT)(_C-5n=nv"/t~4zBGtnx8/.YTSU')), 'mcpT)(_C-5n=nv"/t~4zBGtnx8/.YTSU');
+
+try {
+    wrongDecrypt(encrypt('mcpT)(_C-5n=nv"/t~4zBGtnx8/.YTSU'));
+} catch(e) {
+    assert.strictEqual(e.message, 'error:06065064:digital envelope routines:EVP_DecryptFinal_ex:bad decrypt');
+}
