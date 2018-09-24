5ada1a7a928ff44dd09a16924d6805c385b58eb8
Felipe <feliperohdee@gmail.com>
2018-09-24 14:43:45 -0300

    some stuffs

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
 const crypto = require('crypto');
 
-module.exports = (key, algorithm = 'aes-256-cbc') => {
+const smallOrangeCrypto = (key, defaultIv, algorithm = 'aes-256-cbc') => {
     if (!key) {
         throw new Error('no key provided.');
     }
 
+    if (!Buffer.isBuffer(key)) {
+        key = Buffer.from(key);
+    }
+    
+    if (defaultIv && !Buffer.isBuffer(defaultIv)) {
+        defaultIv = Buffer.from(defaultIv, 'hex');
+    }
+
     function encrypt(text) {
-        let iv = crypto.randomBytes(16);
-        let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+        let iv = defaultIv;
+
+        if (!iv) {
+            iv = crypto.randomBytes(16);
+        }
+
+        let cipher = crypto.createCipheriv(algorithm, key, iv);
         let encrypted = cipher.update(text);
-    
-        encrypted = Buffer.concat([encrypted, cipher.final()]);
-    
+
+        encrypted = Buffer.concat([
+            encrypted, 
+            cipher.final()
+        ]);
+
         return iv.toString('hex') + ':' + encrypted.toString('hex');
     }
-    
-    function decrypt(text) {
-        let textParts = text.split(':');
-        let iv = Buffer.from(textParts.shift(), 'hex');
-        let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-        let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-        let decrypted = decipher.update(encryptedText);
-    
-        decrypted = Buffer.concat([decrypted, decipher.final()]);
-    
-        return decrypted.toString();
+
+    function decrypt(hash) {
+        let [
+            iv, 
+            text
+        ] = hash.split(':');
+        
+        iv = Buffer.from(iv, 'hex');
+        text = Buffer.from(text, 'hex');
+
+        return decryptBuffer(iv, text);
     }
     
+    function decryptBuffer(iv, buffer) {
+        let decipher = crypto.createDecipheriv(algorithm, key, iv);
+        let decrypted = decipher.update(buffer);
+
+        decrypted = Buffer.concat([
+            decrypted, 
+            decipher.final()
+        ]);
+
+        return decrypted.toString();
+    }
+
     return {
         encrypt,
-        decrypt
+        decrypt,
+        decryptBuffer
     };
 };
+
+smallOrangeCrypto.randomBytes = (size = 16) => {
+    return crypto.randomBytes(size);
+};
+
+module.exports = smallOrangeCrypto;
